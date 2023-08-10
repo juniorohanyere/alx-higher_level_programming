483fb2729396087b4e4657a718f752dc9ee4bd3b
Twin J <junohanyere@gmail.com>
2023-08-10 03:30:57 +0100

    no more that two print functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,12 @@
 def uppercase(str):
     for i in str:
         if ord(i) == ord(' '):
-            print("{:s}".format(chr(ord(i))), end="")
+            i = chr(ord(i))
         elif ord(i) >= ord('a') and ord(i) <= ord('z'):
-            print("{:s}".format(chr(ord(i) - 32)), end="")
+            i = chr(ord(i) - 32)
         elif ord(i) >= ord('A') and ord(i) <= ord('Z'):
-            print("{:s}".format(chr(ord(i))), end="")
+            i = chr(ord(i))
         else:
-            print("{:s}".format(chr(ord(i))), end="")
+            i = chr(ord(i))
+        print("{:s}".format(chr(ord(i))), end="")
     print()
