84744197a8a64912b27abb12c9245b604f503f34
Twin J <junohanyere@gmail.com>
2023-08-11 10:35:23 +0100

    print hidden names

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+
+def print_names(names=dir(hidden_4)):
+    for name in names:
+        if name[:2] == '__':
+            pass
+        else:
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    print_names()
