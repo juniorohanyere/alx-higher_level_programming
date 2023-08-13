fc0c8071b18d9ea38cd8e203e1f63b6cca255601
Twin J <junohanyere@gmail.com>
2023-08-13 21:21:23 +0100

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+
+    if length > 0:
+        first = sentence[0]
+
+    else:
+        first = "None"
+
+    t = length, first
+
+    return (t)
