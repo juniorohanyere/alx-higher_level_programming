e042900bd9bd96622c386b780e8d3ff80fbc21ea
Twin J <junohanyere@gmail.com>
2023-08-13 20:48:09 +0100

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return
-
     for row in matrix:
         for column in row:
             print("{:d}".format(column), end=" ")
