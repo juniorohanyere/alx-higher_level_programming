2224c28b3b1090ca1ddd8abe28437d285b5f36cb
Twin J <junohanyere@gmail.com>
2023-08-16 21:55:23 +0100

    map and lambda

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+'''
+ ' multiply_list_map - function that returns a list with all values multiplied
+ '                     by a number without using any loops
+ '
+ ' @my_list: the list
+ ' @number: the number
+ '
+ ' Return: return a new list
+'''
+
+
+def multiply_list_map(my_list=[], number=0):
+    new_list = list(map(lambda x: x * number, my_list))
+
+    return (new_list)
