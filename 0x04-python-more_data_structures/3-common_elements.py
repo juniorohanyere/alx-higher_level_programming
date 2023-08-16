b3877041b8961f775114aa43d315246fcecbf7a4
Twin J <junohanyere@gmail.com>
2023-08-16 16:48:29 +0100

    common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+'''
+ ' common_elements - function that returns a set of common elements in two sets
+ '
+ ' @set_1: first set
+ ' @set_2: second set
+ '
+ ' Return: return a set of the common elements
+'''
+
+
+def common_elements(set_1, set_2):
+    return (set_1 & set_2)
