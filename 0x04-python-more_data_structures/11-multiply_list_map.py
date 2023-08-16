2a5e176f62b19be6f2a2eb3502340c9917b1790e
Twin J <junohanyere@gmail.com>
2023-08-16 22:01:26 +0100

    reduced the length of file

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-
-
-'''
- ' multiply_list_map - function that returns a list with all values multiplied
- '                     by a number without using any loops
- '
- ' @my_list: the list
- ' @number: the number
- '
- ' Return: return a new list
-'''
-
-
 def multiply_list_map(my_list=[], number=0):
     return (list(map(lambda x: x * number, my_list)))
