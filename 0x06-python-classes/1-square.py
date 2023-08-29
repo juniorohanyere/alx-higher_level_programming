8fc268c73abe7e324be51e19a392e6908cad3887
Twin J <junohanyere@gmail.com>
2023-08-29 14:45:23 +0100

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+''' square with size '''
+
+
+class Square():
+    ''' defines a square '''
+    def __init__(self, size):
+        ''' define a square with a size
+
+            Args:
+                size (int): the size of the square
+        '''
+        self.__size = size
