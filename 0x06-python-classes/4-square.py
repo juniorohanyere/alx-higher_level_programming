af31f6d8c4c83344c02660b26a4c7c01069db6ed
Twin J <junohanyere@gmail.com>
2023-08-29 15:51:40 +0100

    getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+''' square with size '''
+
+
+class Square:
+    ''' defines a square '''
+
+    def __init__(self, size=0):
+        ''' define a square with a size
+
+            Args:
+                size (int): the size of the square
+        '''
+
+        self.size = size
+
+    @property
+    def size(self):
+        ''' retrieves size of square '''
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        ''' sets the size for a square
+
+            Args:
+                value (int): the value of the size to set
+        '''
+
+        if not isinstance(value, int):
+            raise (TypeError("size must be an integer"))
+
+        elif value < 0:
+            raise (ValueError("size must be >= 0"))
+
+        self.__size = value
+
+    def area(self):
+        ''' area of the square '''
+
+        return (self.__size ** 2)
