5ed5afd51fef5a912c80a73ef810dacdbc8cb507
Twin J <junohanyere@gmail.com>
2023-09-03 16:24:02 +0100

    say my name!

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,9 +18,9 @@ def matrix_divided(matrix, div):
     if (matrix == [] or not isinstance(matrix, list) or not
         all(isinstance(row, list) for row in matrix) or not
         all((isinstance(element, int) or isinstance(element, float))
-        for element in [value for row in matrix for value in row])):
+            for element in [value for row in matrix for value in row])):
         raise (TypeError("matrix must be a matrix (list of lists) of integers"
-                          + "/floats"))
+                         + "/floats"))
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise (TypeError("Each row of the matrix must have the same size"))
