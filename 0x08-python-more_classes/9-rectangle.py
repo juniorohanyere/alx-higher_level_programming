dbde9a38508163cb4cc0fdb110ef5005189c34ed
Twin J <junohanyere@gmail.com>
2023-09-05 14:27:53 +0100

    resolved incorrect output

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -35,9 +35,9 @@ class Rectangle:
                 size (int): the size of the rectangle (optional)
         '''
 
-        if size < 0:
-            raise ValueError("width must be >= 0")
-        return cls(size, size)
+        cls.__height = size
+        cls.__width = size
+        return Rectangle(cls.__width, cls.__height)
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
