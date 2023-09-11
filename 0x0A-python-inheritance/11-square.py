9c6a272a7c45e07f5d8da5d24616f4c53b448aec
Twin J <junohanyere@gmail.com>
2023-09-11 20:46:29 +0100

    rectangle

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+    defines a square rectangle class
+'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''
+        square rectangle class
+
+        Args:
+            Rectangle: a rectangle object
+    '''
+
+    def __init__(self, size):
+        '''
+            initialiser
+        '''
+
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
