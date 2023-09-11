147c87cb1e2aaca8be5b8a429f6a0b73facd38a0
Twin J <junohanyere@gmail.com>
2023-09-11 15:56:08 +0100

    empty base geometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+    defines an empty class BaseGeometry
+'''
+
+
+class BaseGeometry:
+    '''
+        BaseGeometry: an empty class
+
+        Return: return nothing
+    '''
+
+    pass
