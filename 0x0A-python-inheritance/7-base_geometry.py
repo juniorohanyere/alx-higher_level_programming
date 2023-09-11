5e7f088a291a51845adad77ac20daabced9b4772
Twin J <junohanyere@gmail.com>
2023-09-11 17:09:31 +0100

    base geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+'''
+    6-base_geometry - defines a base geometry class
+'''
+
+
+class BaseGeometry:
+    '''
+        BaseGeometry - a class based on 6-base_geometry.py
+    '''
+
+    def area(self):
+        '''
+            area - area of a geometry
+
+            Return: raises an exception (area if not implemented)
+        '''
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''
+            integer_validator - validates values
+
+            Args:
+                name: always a string
+                value: the value of @name (must be an integer)
+
+            Return: raise TypeError if @value is not an integer
+                    raise ValueError if @value is less than or equals zero
+        '''
+
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
