7a48352ed79822e25bcb4f44cde4f42a166f145a
Twin J <junohanyere@gmail.com>
2023-09-11 16:12:47 +0100

    exception in area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+    6-base_geometry - defines a base geometry class
+'''
+
+
+class BaseGeometry:
+    '''
+        BaseGeometry - a class based on 5-base_geometry.py
+    '''
+
+    def area(self):
+        '''
+            area - area of a geometry
+
+            Return: raises an exception (area if not implemented)
+        '''
+
+        raise Exception("area() is not implemented")
