8b2fca0ee31226c4011e51d425e72b06a611d08f
Twin J <junohanyere@gmail.com>
2023-10-17 18:42:26 +0100

    resolved typo error

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- lists all records within a specific range in a database table in a MySQL
 -- server
 
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score, DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
