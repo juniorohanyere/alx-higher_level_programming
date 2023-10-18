9088395ddf3685536434002e0f67b92ca8a14dc8
Twin J <junohanyere@gmail.com>
2023-10-18 21:30:38 +0100

    list all comedy shows in a database

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,13 @@
+-- lists all shows, and all genres linked to that particular show, from a
+-- particular database
+
+SELECT tv_show.`title`
+    FROM `tv_shows` AS tv_show
+        INNER JOIN `tv_show_genres` AS state
+        ON tv_show.`id` = state.`show_id`
+
+        INNER JOIN `tv_genres` AS genre
+        ON genre.`id` = state.`genre_id`
+
+        WHERE genre.`name` = 'Comedy'
+    ORDER BY tv_show.`title`;
