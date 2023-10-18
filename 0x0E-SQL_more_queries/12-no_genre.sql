1654f97093a89fc26fb3ce6a36061d051a0a9461
Twin J <junohanyere@gmail.com>
2023-10-18 20:55:54 +0100

    resolved

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,4 +4,5 @@ SELECT state.`title`, genre.`genre_id`
     FROM `tv_shows` AS state
         LEFT JOIN `tv_show_genres` AS genre
         ON state.`id` = genre.`show_id`
+        WHERE genre.`genre_id` IS NULL
     ORDER BY state.`title`, genre.`genre_id`;
