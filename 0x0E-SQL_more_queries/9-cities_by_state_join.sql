4f78fa59a3cc56f69fd47ffead656b9eeccb3d6a
Twin J <junohanyere@gmail.com>
2023-10-18 19:49:43 +0100

    resolved

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,5 @@
 SELECT city.`id`, city.`name`, state.`name`
     FROM `cities` AS city
         INNER JOIN `states` AS state
-        ON city.`state_id` = city.`id`
+        ON city.`state_id` = state.`id`
     ORDER BY city.`id`;
