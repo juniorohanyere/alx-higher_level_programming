05fe65e4cff4df322f8ff2740b75c2291d5ad313
Twin J <junohanyere@gmail.com>
2023-10-18 20:44:37 +0100

    list all show contained in a database without a genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in a particular database without a genre linked
+
+SELECT state.`title`, genre.`genre_id`
+    FROM `tv_shows` AS state
+        LEFT JOIN `tv_show_genres` AS genre
+        ON state.`id` = genre.`show_id`
+    ORDER BY state.`title`, genre.`genre_id`;
