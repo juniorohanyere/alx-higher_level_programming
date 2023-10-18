0e4f266c62716f247dd1ac0e72cd39e5759d0531
Twin J <junohanyere@gmail.com>
2023-10-18 04:11:42 +0100

    resolved typo error

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,7 +3,7 @@
 
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE `month` = 7 OR `mont` = 8
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
