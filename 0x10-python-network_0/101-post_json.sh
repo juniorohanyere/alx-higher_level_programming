1ce5abcd3037f188538ff450deeda1a57af6a5b2
Twin J <junohanyere@gmail.com>
2023-11-30 15:06:31 +0100

    bug fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST -d "@$2" -N "Content-Type: application/json" "$1"
+curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2" 
