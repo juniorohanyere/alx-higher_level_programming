b31e7fbaaff36229cb27654efb43ff696ca1f038
Twin J <junohanyere@gmail.com>
2023-12-01 13:50:37 +0100

    display the id of a github user using the github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,11 +5,11 @@ to display the id
 
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(sys.argv[1])
-    request = requests.get(url, auth=requests.auth.HTTPBasicAuth(sys.argv[1],
-                                                                 sys.argv[2]))
+    request = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
 
     print(request.json().get('id'))
